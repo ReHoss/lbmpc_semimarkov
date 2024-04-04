c4bcf5b18a91f8348a2f7a619008b8d5382c6c0c
Rémy Hosseinkhan <hosseinkhan@lisn.fr>
2024-04-04 23:03:03 +0200

    Remove from index

--- barl/policies/__init__.py
+++
@@ -1,2 +0,0 @@
-from barl.policies.mlp_policy import MlpPolicy, TanhMlpPolicy
-from barl.policies.mpc_policy import BayesMPCPolicy
--- barl/util/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Various utilities.
-"""
-
-from . import control_util, domain_util, timing, misc_util
-
-__all__ = [control_util, domain_util, misc_util]
